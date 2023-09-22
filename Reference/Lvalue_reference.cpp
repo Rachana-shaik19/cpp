820f8d23d3db4943bd1ab4954369ca53fb7e712b
Rachana-shaik19 <rachanashaik04@gmail.com>
2023-09-22 15:22:53 +0530

    Reference

---
+++ Reference/Lvalue_reference.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int l_value()
+{
+
+    return 0;
+}
+int main()
+{
+    int val = 10;
+    int &val_ptr = val;
+    std::cout<<"val = "<< 
+    val_ptr = 21;
+
+    return 0;
+}
